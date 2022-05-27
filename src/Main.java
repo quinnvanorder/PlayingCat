b58db78dfff22d47b93348573b33864d281d92a4
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-27 13:34:30 -0700

    Created framework and detailed challenge in main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+The cats spend most of the day playing. In particular, they play if the temperature is between 25 and 35 inclusive.
+Unless it is summer, then the upper limit is 45 inclusive instead of 35.
+
+Write a method isCatPlaying that has 2 parameters. Method needs to return true if the cat ia playing.
+1st parameter should be boolean named summer
+2nd parameter represents the temperature and is of type int.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
